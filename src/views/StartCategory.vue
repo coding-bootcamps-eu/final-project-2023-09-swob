3af7505865699394665ea0a565d7ad40bd180dcd
Mimi Bak <bak.mihyeong@gmail.com>
2024-01-09 10:32:55 +0100

    add:Fallbacktext to StartCategory view

--- src/views/StartCategory.vue
+++ src/views/StartCategory.vue
@@ -4,34 +4,38 @@
     <HamburgerMenu />
   </header>
   <main>
-    <div v-if="isLoggedIn">
-      <h1>Welcome back, {{ currentUser.firstname }}</h1>
-      <h2 v-html="whatKindOfJobs"></h2>
+    <div v-if="showBackupData">
+      <h1>Mensch, es ist ein Fallback</h1>
     </div>
     <div v-else>
-      <img src="@/assets/icons/steps01.svg" alt="step1" class="svg-icon" />
-      <h1 v-html="whatKindOfJobs"></h1>
+      <div v-if="isLoggedIn">
+        <h1>Welcome back, {{ currentUser.firstname }}</h1>
+        <h2 v-html="whatKindOfJobs"></h2>
+      </div>
+      <div v-else>
+        <img src="@/assets/icons/steps01.svg" alt="step1" class="svg-icon" />
+        <h1 v-html="whatKindOfJobs"></h1>
+      </div>
+      <ul>
+        <li v-for="item in categoryStore.categorys" :key="item.id">
+          <article class="card" :style="{ 'background-image': 'url(' + item.imageSrc + ')' }">
+            <div class="overlay">
+              <section class="content">
+                <router-link
+                  :to="{
+                    name: 'SwipeScreen',
+                    query: { category: item.id, categoryName: item.caption }
+                  }"
+                  class="button"
+                >
+                  <p class="card-title">{{ item.caption }}</p>
+                </router-link>
+              </section>
+            </div>
+          </article>
+        </li>
+      </ul>
     </div>
-    <ul>
-      <li v-for="item in categoryStore.categorys" :key="item.id">
-        <article class="card" :style="{ 'background-image': 'url(' + item.imageSrc + ')' }">
-          <div class="overlay">
-            <section class="content">
-              <router-link
-                :to="{
-                  name: 'SwipeScreen',
-                  query: { category: item.id, categoryName: item.caption }
-                }"
-                class="button"
-              >
-                <p class="card-title">{{ item.caption }}</p>
-              </router-link>
-              >
-            </section>
-          </div>
-        </article>
-      </li>
-    </ul>
   </main>
 </template>
 
@@ -44,7 +48,8 @@ export default {
     return {
       whatKindOfJobs: 'What kind of job would you like to get?',
       isLoggedIn: false,
-      currentUser: JSON.parse(sessionStorage.getItem('credentials'))
+      currentUser: JSON.parse(sessionStorage.getItem('credentials')),
+      showBackupData: false
     }
   },
   setup() {
@@ -67,6 +72,10 @@ export default {
         })
         .then((jsonData) => {
           this.categoryStore.initCategory(jsonData)
+        })
+        .catch((error) => {
+          console.error('Error loading categories:', error.message)
+          this.showBackupData = true
         })
     },
     checkLoggedUser() {

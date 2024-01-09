965e2bd8e37bff7d8c9ec4ea0343dc8bcb73f4f3
Mimi Bak <bak.mihyeong@gmail.com>
2024-01-09 15:26:18 +0100

    add:API to buttons swipe modaljs deleted bodytags

--- src/views/SwipeScreen.vue
+++ src/views/SwipeScreen.vue
@@ -1,110 +1,115 @@
 <template>
-  <body>
-    <div>
-      <header>
-        <img class="nav-logo" src="@/assets/icons/Logo_180x180_light.png" alt="SWOB Logo" />
-        <HamburgerMenu />
-      </header>
+  <div>
+    <header>
+      <img class="nav-logo" src="@/assets/icons/Logo_180x180_light.png" alt="SWOB Logo" />
+      <HamburgerMenu />
+    </header>
 
-      <main>
-        <form
-          @submit.prevent
-          @mousedown="handleMouseDown"
-          @mousemove="handleMouseMove"
-          @mouseup="handleMouseUp"
+    <main>
+      <form
+        @submit.prevent
+        @mousedown="handleMouseDown"
+        @mousemove="handleMouseMove"
+        @mouseup="handleMouseUp"
+      >
+        <div
+          class="swipe-container"
+          :style="{ transform: `translateX(${currentX}px) rotate(${rotation}deg)` }"
+          ref="swipeContainer"
         >
-          <div
-            class="swipe-container"
-            :style="{ transform: `translateX(${currentX}px) rotate(${rotation}deg)` }"
-            ref="swipeContainer"
-          >
-            <div class="title-company-img-container">
-              <!-- Jobbeschreibung -->
-              <div class="job-description-container">
-                <div class="job-description">
-                  {{ offersStore.offers[currentIndex].title }}
-                </div>
-                <div class="company-logo-container">
-                  <img
-                    class="company-logo"
-                    :src="companysStore.companys[currentIndex].logo"
-                    alt="Company Logo"
-                  />
-                </div>
+          <div class="title-company-img-container">
+            <!-- Jobbeschreibung -->
+            <div class="job-description-container">
+              <div class="job-description">
+                {{ offersStore.offers[currentIndex].title }}
               </div>
-              <!-- Company Name -->
-              <h3 class="company-name">{{ companysStore.companys[currentIndex].name }}</h3>
-            </div>
-            <!-- Filter -->
-            <div class="filter-section">
-              <label for="selectedCategory" class="category-label">{{
-                $route.query.categoryName
-              }}</label>
-              <label for="filter1" class="filter-label">{{ filtersStore.howWork }}</label>
-            </div>
-
-            <!-- Infobox für Firmeninformationen -->
-            <div class="info-box-container">
-              <p class="info-box" v-if="!showMoreInfo">
-                {{ truncateDescription(offersStore.offers[currentIndex].description, 300) }}
-                <button @click="toggleMoreInfo" class="more-info-button">
-                  Mehr anzeigen <i class="fas fa-chevron-down"></i>
-                </button>
-              </p>
-            </div>
-            <div v-show="showMoreInfo" class="more-info-content" ref="moreInfoContent">
-              <span class="close" @click="toggleMoreInfo">&times;</span>
-              <div class="modal-content info-box-content">
-                <p>{{ offersStore.offers[currentIndex].description }}</p>
+              <div class="company-logo-container">
+                <img
+                  class="company-logo"
+                  :src="companysStore.companys[currentIndex].logo"
+                  alt="Company Logo"
+                />
               </div>
             </div>
-
-            <!-- Standort -->
-            <div class="location-section">
-              <p class="workplace">Workplace</p>
-              <p class="location">{{ profiles[currentIndex].location }}</p>
-            </div>
-
-            <!-- Bildergalerie -->
-            <div class="gallery">
-              <img
-                v-for="image in profiles[currentIndex].gallery"
-                :src="image"
-                :key="image"
-                @click="openLightbox(image)"
-                alt="Gallery Image"
-              />
-            </div>
-
-            <!-- Lightbox -->
-            <div v-if="lightboxImage" @click="closeLightbox" class="lightbox">
-              <img :src="lightboxImage" alt="Enlarged Image" />
-            </div>
+            <!-- Company Name -->
+            <h3 class="company-name">{{ companysStore.companys[currentIndex].name }}</h3>
+          </div>
+          <!-- Filter -->
+          <div class="filter-section">
+            <label for="selectedCategory" class="category-label">{{
+              $route.query.categoryName
+            }}</label>
+            <label for="filter1" class="filter-label">{{ filtersStore.howWork }}</label>
           </div>
 
-          <div class="swipe-buttons">
-            <button @click="swipeLeft">
-              <img src="@/assets/icons/no.png" alt="Swipe Left Icon" />
-            </button>
-            <!-- <button @click="swipeRight">
-              <img src="@/assets/icons/yes.png" alt="Swipe Right Icon" />
-            </button> -->
-            <div class="link-to-match">
-              <router-link to="/match" id="match"
-                ><img class="icon-green" src="@/assets/icons/yes.png" alt="Swipe Right Icon"
-              /></router-link>
-            </div>
+          <!-- Infobox für Firmeninformationen -->
+          <div class="info-box-container">
+            <p class="info-box" v-if="!showMoreInfo">
+              {{ truncateDescription(offersStore.offers[currentIndex].description, 300) }}
+              <button
+                @click="toggleMoreInfo(offersStore.offers[currentIndex].description)"
+                class="more-info-button"
+              >
+                Mehr anzeigen <i class="fas fa-chevron-down"></i>
+              </button>
+            </p>
           </div>
-        </form>
-      </main>
-    </div>
-  </body>
+          <!-- <div v-show="showMoreInfo" class="more-info-content" ref="moreInfoContent">
+            <span class="close" @click="toggleMoreInfo">&times;</span>
+            <div class="modal-content info-box-content">
+              <p>{{ offersStore.offers[currentIndex].description }}</p>
+            </div>
+          </div> -->
+
+          <!-- Standort -->
+          <div class="location-section">
+            <p class="workplace">Workplace</p>
+            <p class="location">{{ profiles[currentIndex].location }}</p>
+          </div>
+
+          <!-- Bildergalerie -->
+          <div class="gallery">
+            <img
+              v-for="image in profiles[currentIndex].gallery"
+              :src="image"
+              :key="image"
+              @click="openLightbox(image)"
+              alt="Gallery Image"
+            />
+          </div>
+
+          <!-- Lightbox -->
+          <div v-if="lightboxImage" @click="closeLightbox" class="lightbox">
+            <img :src="lightboxImage" alt="Enlarged Image" />
+          </div>
+        </div>
+
+        <div class="swipe-buttons">
+          <button @click="swipeLeft">
+            <img src="@/assets/icons/no.png" alt="Swipe Left Icon" />
+          </button>
+          <div class="link-to-match">
+            <!-- Wenn ein match successful ist, routet er zum MatchScreen -->
+            <router-link
+              :to="{
+                name: 'match'
+              }"
+              @click="swipeRight"
+              class="button"
+              ><img class="icon-green" src="@/assets/icons/yes.png" alt="Swipe Right Icon"
+            /></router-link>
+          </div>
+        </div>
+      </form>
+    </main>
+  </div>
 </template>
 
 <script>
 import { useOfferStore } from '@/stores/offers.js'
 import { useCompanyStore } from '@/stores/company.js'
 import { useFiltersStore } from '@/stores/filter.js'
+import { useModalStore } from '@/stores/modal.js'
 import blancaImage from '@/assets/workplaces/annie-spratt-FSFfEQkd1sc-unsplash.jpeg'
 import mimiImage from '@/assets/workplaces/annie-spratt-FSFfEQkd1sc-unsplash.jpeg'
 import giulioImage from '@/assets/workplaces/redd-f-5U_28ojjgms-unsplash.jpeg'
@@ -114,6 +119,7 @@ export default {
     const filtersStore = useFiltersStore()
     const offersStore = useOfferStore()
     const companysStore = useCompanyStore()
+    const modalStore = useModalStore()
 
     const sessionFilters = JSON.parse(sessionStorage.getItem('filters'))
 
@@ -121,10 +127,11 @@ export default {
       filtersStore.howWork = sessionFilters[0].howWork
     }
 
-    return { filtersStore, offersStore, companysStore }
+    return { filtersStore, offersStore, companysStore, modalStore }
   },
   data() {
     return {
+      matches: [],
       profiles: [
         {
           image: blancaImage,
@@ -151,7 +158,7 @@ export default {
     }
   },
   created() {
-    this.loadOffers(), this.loadCompanys()
+    this.loadOffers(), this.loadCompanys(), this.loadMatches()
   },
   methods: {
     loadOffers() {
@@ -194,11 +201,65 @@ export default {
         return description
       }
     },
+    loadMatches() {
+      fetch('http://localhost:3000/matches/')
+        .then((response) => {
+          if (response.ok) {
+            return response.json()
+          } else {
+            throw new Error('Load matches was not ok.')
+          }
+        })
+        .then((jsonData) => {
+          this.matches = jsonData
+          console.log(jsonData)
+        })
+    },
     swipeLeft() {
       this.currentIndex = (this.currentIndex + 1) % this.profiles.length
     },
     swipeRight() {
       this.currentIndex = (this.currentIndex - 1 + this.profiles.length) % this.profiles.length
+      this.currentIndex = (this.currentIndex - 1 + this.profiles.length) % this.profiles.length
+
+      this.createMatch()
+        .then(() => {
+          console.log('Match created successfully')
+
+          // Hier wird die Navigation zum MatchScreen ausgelöst
+          this.$router.push({ name: 'match' })
+        })
+        .catch((error) => {
+          console.error('Failed to create a match:', error)
+        })
+    },
+    createMatch() {
+      const changeMatch = {
+        id: this.matches[this.currentIndex].id,
+        userId: this.matches[this.currentIndex].userId,
+        companieId: this.matches[this.currentIndex].companyId,
+        offerId: this.matches[this.currentIndex].offerId,
+        isMatch: true
+      }
+      console.log(this.matches[this.currentIndex].id)
+
+      return fetch('http://localhost:3000/matches/' + this.matches[this.currentIndex].id, {
+        method: 'PUT',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify(changeMatch)
+      })
+        .then((response) => {
+          if (response.ok) {
+            return Promise.resolve() // Resolve the promise if the request was successful
+          } else {
+            return Promise.reject('Failed to create a match') // Reject the promise if there was an error
+          }
+        })
+        .catch((error) => {
+          return Promise.reject(error) // Propagate the error to the outer catch block
+        })
     },
     handleMouseDown(event) {
       if (this.$el.contains(event.target)) {
@@ -233,7 +294,10 @@ export default {
     setSwipeCursor(cursorStyle) {
       this.$refs.swipeContainer.style.cursor = cursorStyle
     },
-    toggleMoreInfo() {
+    toggleMoreInfo(description) {
+      this.modalStore.open = true
+      this.modalStore.content = description
+
       this.showMoreInfo = !this.showMoreInfo
       if (this.showMoreInfo) {
         document.body.style.overflow = 'hidden'
